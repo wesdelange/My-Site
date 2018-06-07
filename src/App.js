9225c1973814ac05b018d5e17fb97be8d222fe95
Wes <wes.delange@gmail.com>
2018-06-07 14:20:42 -0400

    Styled about section and portfolio section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import Aloha from './images/aloha-screenshot.png';
 import Instanews from './images/instanews-screenshot.png';
 import Inhabitent from './images/inhabitent-screenshot.png';
@@ -25,58 +24,67 @@ class App extends Component {
         
         <div className="About"> 
           <h1>About Me</h1>
-          <h3>Currently studying Web Development at RED Academy</h3>
+          <h2>Currently studying Web Development at RED Academy</h2>
           <p className="My-intro">
-            My journey into the world of web development has just begun.
-            A strong desire to learn new skills and expand my realm of competence has lead me to RED Academy.
+              My journey into the world of web development has just begun.
+              A strong desire to learn new skills and expand my realm of competence has lead me to RED Academy.
+              Looking to break into the exciting Tech Industry as a Jr Front-End Web Developer.
+              Extremely interested in exploring Artificial Intelligence and Deep Learning as well as the emerging science of complexity.
+              My work experiences include being a Fly-Fishing Guide, Dog-Sledding Guide, Carpenter, Fisheries Technician and Bicycle Courier.
+              Major accomplishments in my life include following my passion of adventure and completing two 10000km+ solo bicycle touring trips.
+              In 2016 I cycled across Canada from the Prince Rupert, BC to St.John's, NFLD and back to Toronto, ON.
+              The next summer I biked all over BC and the Yukon, as well as a huge chunk of Alaska.
+              I am currently acquiring the necessary skills to further my competence in the digital world.
+              I also enjoy philosophy and have fun attempting to grasp the true nature of reality in my spare time.
+              I enjoy staying fit and eating healthy while constantly working at self actualization.
           </p>
         </div>
 
-        <h1>Portfolio</h1>
+        
         <div className="My-portfolio">
-
+        <h1>Portfolio</h1>
           
           <div  id="aloha" className="project">
-            <h1>Aloha</h1>
+            <h2>Aloha</h2>
             <div className="container">
               <a href="https://wesdelange.github.io/Aloha/" target="_blank">
                 <img src={Aloha} alt="Aloha"/>
               </a>
             </div>
-            <p>Responsive webpage for mock fashion company</p>
+            <h3>Responsive webpage for mock fashion company</h3>
             <a href="https://github.com/wesdelange/Aloha">View code on GitHub</a>
           </div>
           
           <div id="instanews" className="project">
-            <h1>Instanews</h1>
+            <h2>Instanews</h2>
             <div className="container">
               <a href="https://wesdelange.github.io/instanews/" target="_blank">
                 <img src={Instanews} alt="Instanews"/>
               </a>
             </div>
-            <p>Responsive webpage displaying today's top news stories fetched from New York Times API</p>
+            <h3>Responsive webpage displaying today's top news stories fetched from New York Times API</h3>
             <a href="https://github.com/wesdelange/instanews">View code on GitHub</a>
           </div>
 
           <div id="inhabitent" className="project">
-            <h1>Inhabitent</h1>
+            <h2>Inhabitent</h2>
             <div className="container">
               <a href="" target="_blank">
                 <img src={Inhabitent} alt="Inhabitent"/>
               </a>
             </div>
-            <p>Wordpress webpage with custom theme and plug-ins</p>
+            <h3>Wordpress webpage with custom theme and plug-ins</h3>
             <a href="https://github.com/wesdelange/inhabitent">View code on GitHub</a>
           </div>
 
           <div id="qod" className="project">
-            <h1>Quotes-on-Dev</h1>
+            <h2>Quotes-on-Dev</h2>
             <div className="container">
               <a href="" target="_blank">
                 <img src={Qod} alt="Quotes-on-Dev"/>
               </a>
             </div>
-            <p>Responsive Custom Wordpress webpage that utilizes Rest API to display random quotes</p>
+            <h3>Responsive Custom Wordpress webpage that utilizes Rest API to display random quotes</h3>
             <a href="https://github.com/wesdelange/inhabitent">View code on GitHub</a>
           </div>
 
